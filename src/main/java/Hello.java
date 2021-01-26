7d092ff852a4b1f37a141b9d3594cc83ab54ece2
Vida Duodu <vidash50@gmail.com>
2021-01-26 13:43:32 -0500

    B21 learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
 
-
         System.out.println("Hello world");
+
+        System.out.println("B21 future SDET learning Git");
     }
 }
